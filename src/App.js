328940fe3f468237ee303b4a01b51a351d62c464
maca-plaza <165829605+maca-plaza@users.noreply.github.com>
2024-05-29 17:44:33 -0400

    cambios en estiilos

--- src/App.js
+++ src/App.js
@@ -23,8 +23,9 @@ export default function App() {
           <Route path= "residuos" element ={ <Residuos />}/>
           <Route path="energias" element={<Energias />}>
             <Route path="calculadora" element={<Calculadora />}/>
-            <Route path="productos" element={<Productos />}/>
+            
           </Route>
+          <Route path="productos" element={<Productos />}/>
 
         </Routes>
         <Footer />
