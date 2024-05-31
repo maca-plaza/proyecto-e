f7ba320b294d75420b5d7424a8cbb65cd585fa54
maca-plaza <165829605+maca-plaza@users.noreply.github.com>
2024-05-31 15:23:33 -0400

    cambios en footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,35 +8,9 @@ const Footer = () => {
         <div className="logo">
           <img src="eficiencia energetica/LogoBlanco.png" alt="" />
         </div>
-        <form id="form">
-          <div className="subtitulo_formulario">
-            <h1>¿Quieres ofrecer tus servicios?</h1>
-            <p>Registra tus datos y nos pondremos en contacto contigo</p>
-          </div>
-          <div className="mb-0">
-            <label for="exampleFormControlInput1" class="form-label"></label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="ejemplo@gmail.com"
-            />
-          </div>
-          <div className="mb-0">
-            <label for="exampleFormControlTextarea1" class="form-label"></label>
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              rows="3"
-              required
-              placeholder="Cuéntanos brevemente sobre tus productos"
-            ></textarea>
-            <button type="submit" class="btn">
-              Enviar
-            </button>
-            <p className="warnings" id="warnings"></p>
-          </div>
-        </form>
+          <form id="form">
+            <Form />
+          </form>
         <div className="contacto">
           <h2>Síguenos</h2>
           <div className="redes_contacto">
