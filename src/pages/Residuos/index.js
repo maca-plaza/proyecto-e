4926a5f1d772f6632416a4f3d990009b38848ff9
Gmunozcaro <gabriela.munoz@genstudents.org>
2024-05-31 13:06:28 -0400

    1

--- src/pages/Residuos/index.js
+++ src/pages/Residuos/index.js
@@ -9,10 +9,11 @@ const Residuos = () => {
             <div class="overlap">
               <img
                 class="image"
-                src="imagenes/contenedor-plastico-restos-basura.jpg"
+                src="https://images.unsplash.com/photo-1510251197878-a2e6d2cb590c?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               />
-              <div class="text-wrapper">Gestión de</div>
-              <div class="text-wrapper-2">Residuos</div>
+              <div class="text-wrapper">
+                Gestión de <span class="residuos">Residuos</span>
+              </div>
             </div>
           </div>
         </div>
@@ -20,35 +21,89 @@ const Residuos = () => {
       <section class="container-xxl">
         <span class="Conoces-la-diferencia-entre-basura-y-residuos-">
           ¿Conoces la diferencia entre
-          <span class="text-style-1">basura</span>y
-          <span class="text-style-2">residuos</span>?
+          <span class="text-style-1"> basura</span> y
+          <span class="text-style-2"> residuos</span>?
         </span>
 
-        <div class="d-flex flex-row-reverse">
+        {/* <div class="d-flex flex-row-reverse">
           <img
-            class="image-110"
-            src="imagenes/basura-toneladas-planeta-cambio-climatico-destruccion-reciclaje-A - copia.jpg"
-          />
-          <div class="align-self-center">
+            class="image-110" src="https://images.unsplash.com/photo-1592890278983-18616401d4ed?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+          <div class="align-self-left">
             <span class="text-style-3">Basura:</span>
-            La basura es todo aquel físico considerado como desecho y que se
-            necesita eliminar. La basura es un producto de las actividades
-            humanas al cual se le considera de valor igual a cero por el
-            desechado, no necesariamente debe ser odorífica, repugnante e
-            indeseable, eso depende del origen y composición de estado que ya no
-            se le puede dar otro uso o no tiene ninguna utilidad posible
+            La basura es todo aquel físico considerado como desecho y que se necesita eliminar. La basura es un producto de las actividades humanas al cual se le considera de valor igual a cero por el desechado, no necesariamente debe ser odorífica, repugnante e indeseable, eso depende del origen y composición de estado que ya no se le puede dar otro uso o no tiene ninguna utilidad posible
           </div>
           <div class="d-flex flex-row mb-3">
-            <img class="image-111" src="imagenes/18.jpg" />
+            <img class="image-111" src="https://images.unsplash.com/photo-1469326656011-29c6b0ccb641?q=80&w=2639&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
             <div class="align-self-center">
+              <span class="text-style-3">Residuos:</span>
+              Los residuos son materiales, sustancias o productos que ya no son útiles para el propósito original para el que fueron creados o utilizados. Estos pueden ser sólidos, líquidos o gaseosos y pueden ser el resultado de actividades humanas o procesos naturales.Los residuos pueden clasificarse de diversas maneras:
+            </div>
+          </div>
+        </div> */}
+
+        {/* <div class="container">
+          <div class="row">
+            <div class="col-sm-5 col-md-6">
+              <div>
+                <span class="text-style-3">Basura:</span>
+                La basura es todo aquel físico considerado como desecho y que se
+                necesita eliminar. La basura es un producto de las actividades
+                humanas al cual se le considera de valor igual a cero por el
+                desechado, no necesariamente debe ser odorífica, repugnante e
+                indeseable, eso depende del origen y composición de estado que
+                ya no se le puede dar otro uso o no tiene ninguna utilidad
+                posible
+              </div>
+            </div>
+            <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
+              <img
+                class="image-110"
+                src="https://images.unsplash.com/photo-1592890278983-18616401d4ed?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-6 col-md-5 col-lg-6">
+              <img
+                class="image-111"
+                src="https://images.unsplash.com/photo-1469326656011-29c6b0ccb641?q=80&w=2639&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              />
+            </div>
+            <div class="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">
               <span class="text-style-3">Residuos:</span>
               Los residuos son materiales, sustancias o productos que ya no son
               útiles para el propósito original para el que fueron creados o
               utilizados. Estos pueden ser sólidos, líquidos o gaseosos y pueden
-              ser el resultado de actividades humanas o procesos naturales. Los
+              ser el resultado de actividades humanas o procesos naturales.Los
               residuos pueden clasificarse de diversas maneras:
             </div>
           </div>
+        </div> */}
+        <div class="basura-1">
+          <img class="image-110" src="https://images.unsplash.com/photo-1592890278983-18616401d4ed?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="imagen"></img>
+          <div class="basura">
+          <span class="text-style-3">Basura: </span>
+                La basura es todo aquel físico considerado como desecho y que se
+                necesita eliminar. La basura es un producto de las actividades
+                humanas al cual se le considera de valor igual a cero por el
+                desechado, no necesariamente debe ser odorífica, repugnante e
+                indeseable, eso depende del origen y composición de estado que
+                ya no se le puede dar otro uso o no tiene ninguna utilidad
+                posible
+          </div>
+
+        </div>
+        <div class="residuos-1">
+          <img class="image-111" src="https://images.unsplash.com/photo-1469326656011-29c6b0ccb641?q=80&w=2639&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="imagen"></img>
+          <div class="residuos-2">
+          <span class="text-style-3">Residuos: </span>
+              Los residuos son materiales, sustancias o productos que ya no son
+              útiles para el propósito original para el que fueron creados o
+              utilizados. Estos pueden ser sólidos, líquidos o gaseosos y pueden
+              ser el resultado de actividades humanas o procesos naturales.Los
+              residuos pueden clasificarse de diversas maneras:
+          </div>
+
         </div>
 
         <span class="Segn-sus-caractersticas">
@@ -107,7 +162,7 @@ const Residuos = () => {
         <div class="container text-center">
           <div class="row row-cols-3 row-cols-lg-3 g-2 g-lg-3">
             <div class="col">
-              <img class="gris-claro" src="imagenes/GRIS CLARO.png" />
+              <img class="gris-claro" src="https://lh3.google.com/u/0/d/1npLbmuLiJ195eeNnIg-eLOkXs2rrL3T3=w2560-h1118-iv1" />
             </div>
             <div class="col">
               <img class="verde" src="imagenes/VERDE.png" />
