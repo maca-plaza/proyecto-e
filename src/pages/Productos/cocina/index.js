3b52ea9fc93db5447d658180db87f49d3b4bf0ab
EvelinGen24 <evelin.veliz@genstudents.org>
2024-06-12 13:19:10 -0400

    mjs

---
+++ src/pages/Productos/cocina/index.js
@@ -0,0 +1,43 @@
+import "./cocina.css";
+
+const Cocina = () => {
+  return (
+    <div className="productos">
+      <div className="div">
+        <div className="overlap">
+          <img className="image" alt="Image" src="image-83.png" />
+          <div className="hero-banner" />
+          <img className="img" alt="Image" src="image-41.png" />
+          <div className="text-wrapper">ESTUCHE REUTILIZABLE DE ALGODÓN</div>
+          <div className="text-wrapper-2">PAÑOS ABSORBENTES</div>
+          <div className="text-wrapper-3">REMOVEDOR DE SARRO</div>
+          <p className="p">KITS DE BOLSAS PARA RECICLAR</p>
+          <img className="image-2" alt="Image" src="image-49.png" />
+          <img className="image-3" alt="Image" src="image-58.png" />
+          <img className="image-4" alt="Image" src="image-61.png" />
+          <div className="text-wrapper-4">$1.800</div>
+          <div className="element">$4.990</div>
+          <div className="text-wrapper-5">$13.000</div>
+          <div className="text-wrapper-6">$16.900</div>
+          <img className="image-5" alt="Image" src="image-64.png" />
+          <img className="image-6" alt="Image" src="image-67.png" />
+          <div className="text-wrapper-7">Cocina</div>
+          <div className="text-wrapper-8">TRAPERO DE TELA RECICLADA</div>
+          <div className="text-wrapper-9">$2.500</div>
+          <div className="text-wrapper-10">LAVALOZA MANDARINA 5L</div>
+          <div className="element-2">$16.990</div>
+          <div className="element-3">$4.500</div>
+          <div className="text-wrapper-11">PRODUCTOS</div>
+          <div className="text-wrapper-12">SOSTENIBLES</div>
+          <div className="group">
+              </div>
+            </div>
+          </div>
+          <img className="left" alt="Left" src="left.png" />
+        </div>
+  );
+};
+
+
+export default Cocina;
+
