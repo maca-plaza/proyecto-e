ea4e4723c8469d62344e35e9dfbe39742c4bd4af
maca-plaza <165829605+maca-plaza@users.noreply.github.com>
2024-06-12 17:42:51 -0400

    cambios en server

---
+++ server/db/models/product.js
@@ -0,0 +1,45 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = new Schema({
+  productId: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    trim: true,
+  },
+  productUrl: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  brand: {
+    type: String,
+    trim: true,
+  },
+});
+
+const Product = model("Product", productSchema);
+
+export default Product;
