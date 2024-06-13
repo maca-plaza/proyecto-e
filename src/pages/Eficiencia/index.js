ef3c0f50aced122ef89e235dde7dbc5ba4fd0d91
Carla-Araya <carla.araya@genstudents.org>
2024-06-13 18:11:26 -0400

    componentes header

--- src/pages/Eficiencia/index.js
+++ src/pages/Eficiencia/index.js
@@ -17,9 +17,6 @@ const Eficiencia = () => {
       <body>
         <main>
           <div>
-            {/* {header.map((h) => (
-              <Header {...h} />
-            ))} */}
             {HEADER.map((c) => (
               <Header {...c} />
             ))}
