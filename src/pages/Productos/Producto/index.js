b4775b544fbedfa5ff8e29fa88e52b50bf8fe1f8
EvelinGen24 <evelin.veliz@genstudents.org>
2024-06-20 19:11:06 -0400

    CSS

--- src/pages/Productos/Producto/index.js
+++ src/pages/Productos/Producto/index.js
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
 const Producto = () => {
@@ -20,7 +19,11 @@ const Producto = () => {
     };
 
     fetchProducto();
-  }, []);
+  }, [id]);
+
+  if (!producto) {
+    return <p>Cargando...</p>; // Muestra un mensaje de carga mientras se obtiene el producto
+  }
 
   const {
     productId,
@@ -31,21 +34,44 @@ const Producto = () => {
     category,
     productUrl,
     brand,
-  } = producto || {};
+  } = producto;
 
   return (
-    <div className={styles["product-container"]}>
-      <div className={styles["product-image-container"]}>
-        <img src={imageUrl} alt={name} />
+    <div className={styles["product-page"]}>
+      <div className={styles["breadcrumb"]}>
+        <Link to={`/productos/categoria/${category}`}>
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/007/231/420/non_2x/arrow-back-icon-which-is-suitable-for-commercial-work-and-easily-modify-or-edit-it-vector.jpg"
+            alt="Back"
+            className={styles["back-arrow"]}
+          />
+        </Link>
+        <span>{category} / {name}</span>
       </div>
-      <div className={styles["product-properties"]}>
-        <h2>{name}</h2>
-        <p>{description}</p>
-        <p>{brand}</p>
-        <p>$ {price}</p>
+      <div className={styles["product-container-22"]}>
+        <div className={styles["product-image-container"]}>
+          <img src={imageUrl} alt={name} />
+        </div>
+        <div className={styles["product-details"]}>
+          <h2>{name}</h2>
+          <p className={styles["product-description"]}>{description}</p>
+          <div className={styles["product-brand-container"]}>
+            <a
+              href={productUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles["product-brand-button"]}
+            >
+              {brand}
+            </a>
+          </div>
+          <p className={styles["product-price"]}>$ {price}</p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Producto;
+
+
