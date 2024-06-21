622a2ecb045952a651494a718e701104500ea61b
Carla-Araya <carla.araya@genstudents.org>
2024-06-20 21:08:06 -0400

    página energias

--- src/pages/Energias/components/Card/index.js
+++ src/pages/Energias/components/Card/index.js
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 const Card = ({ icon, title, paragraph }) => {
   return (
     <div className={styles["card-container"]}>
-      <FontAwesomeIcon icon={icon} size="2x" color="#FBD914" />
+      <FontAwesomeIcon icon={icon} size="2x" color="#fbd914" />
       <h2>{title}</h2>
       <p>{paragraph}</p>
     </div>
