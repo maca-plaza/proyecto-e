dffd0acd25457b42e5d4c4afb0433eea2c6cb488
maca-plaza <165829605+maca-plaza@users.noreply.github.com>
2024-06-25 18:02:50 -0400

    ajsutes varioas

--- src/pages/Wishlist/index.js
+++ src/pages/Wishlist/index.js
@@ -1,56 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom'; // Importa useNavigate
-import './styles.css'; 
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import "./styles.css";
 import Header from "../../components/Header";
-import axios from "axios";
 
-function Wishlist() {
+const Wishlist = () => {
   const HEADER = [
     {
       titulo: "Visita tu lista",
       titulo2: "de favoritos",
       info: "",
     },
-  ];  
-        
+  ];
+
   const [productos, setProductos] = useState([]);
-  
-    useEffect(() => {
-        
-        axios.get('/server/db/models/wishlist.js')
-            .then(response => {
-                setProductos(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching wishlist:', error);
-            });
-    }, []);
 
+  useEffect(() => {
+    const fetchProductos = async () => {
+      try {
+        const response = await fetch("http://localhost:4200/users" + wishlist);
+        if (response.ok) {
+          setProductos(await response.json());
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchProductos();
+  }, [wishlist]);
+
+  if (productos) {
     return (
-    <div className="wishlist-body">
-    <div className="wishlist-Principal">
-      <div className="wishlist-image">
-      <div className="div">
-      <div className="wishlist-image">
-        <div>
-          
-          {HEADER.map((c) => (
-            <Header {...c} />
-          ))}
-        </div>
-    </div>
-    </div>
-        <div className="wishlist-usuarios">
-          <div>Visita tu lista de favoritos</div>
-            <div className="container-list-wishlist">
-           
+      <div className="wishlist-body">
+        <div className="wishlist-Principal">
+          <div className="wishlist-image">
+            <div className="div">
+              <div className="wishlist-image">
+                <div>
+                  {HEADER.map((c) => (
+                    <Header {...c} />
+                  ))}
+                </div>
+              </div>
             </div>
+            <div className="wishlist-usuarios">
+              <div>Visita tu lista de favoritos</div>
+              <div className="container-list-wishlist"></div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-    </div>
     );
-}
-
+  }
+};
 export default Wishlist;
-  
