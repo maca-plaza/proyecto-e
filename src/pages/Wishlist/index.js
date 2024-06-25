ecab26595951371ab8c8f6bd6a7c292bbf2bb810
maca-plaza <165829605+maca-plaza@users.noreply.github.com>
2024-06-25 13:35:29 -0400

    ajustes en axios

--- src/pages/Wishlist/index.js
+++ src/pages/Wishlist/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // Importa useNavigate
 import './styles.css'; 
-import axios from 'axios';
 import Header from "../../components/Header";
 
 function Wishlist() {
