5bedd9cae95a963b7dbbbe5d3245ab3b94734c5b
EvelinGen24 <evelin.veliz@genstudents.org>
2024-06-26 15:08:49 -0400

    game

--- src/pages/Energias/pages/Climatico/index.js
+++ src/pages/Energias/pages/Climatico/index.js
@@ -2,9 +2,6 @@ import React from "react";
 const Climatico = () => {
   return (
     <body>
-      
-      <h2>Cambio climatico</h2>
-
       <iframe
         src="https://cambioclimatico.minenergia.cl/"
         width="100%"
